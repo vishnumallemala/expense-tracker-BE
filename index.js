758c88f5d41865e41cfd4eb3e854a136508c0ee0
vishnumallemala <vishnumallemala3@gmail.com>
2023-10-12 21:09:22 +0530

    Added Dup Swagger

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const userRoutes = require('./routes/user-routes');
 const expenseRoutes = require('./routes/expense-routes');
 const categoryRoutes = require('./routes/refcode/category-routes');
 const currencyRoutes = require('./routes/refcode/currency-routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 app.use(express.json());
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/users', userRoutes);
 app.use('/expenses', expenseRoutes);
 app.use('/categories', categoryRoutes);
